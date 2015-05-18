2aaf424e255d197da98eae11fe77c6f48b17d7d8
unknown <rainbowroad000@yahoo.co.jp>
2015-05-18 10:06:28 +0900

    サーバーとの通信部分おおかた完成

--- client/ImHere/User.cs
+++ client/ImHere/User.cs
@@ -4,10 +4,12 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+/* 各ユーザ情報を格納するクラス */
 namespace ImHere
 {
     public class User
-    {
+
+   {
         public int id { get; set; }
         public string name { get; set; }
         public string lab { get; set; }
